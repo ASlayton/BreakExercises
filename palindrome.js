072b955f26b92152d9b67bfdea0afd673f9db6f6
Amanda Slayton <alslayton86@gmail.com>
2019-01-02 21:53:38 -0600

    setup josephus

--- palindrome.js
+++ palindrome.js
@@ -3,8 +3,8 @@ function isPalindrome () {
   var adjPhrase = phrase.replace(/\s/g, '').toLowerCase();
   var revPhrase = adjPhrase.split("").reverse().join("");
   if (revPhrase === adjPhrase) {
-    document.getElementById("answer").innerHTML = revPhrase + " is a palindrome of " + phrase;
+    document.getElementById("answerPalindrome").innerHTML = revPhrase + " is a palindrome of " + phrase;
   } else {
-    document.getElementById("answer").innerHTML = revPhrase + " is not a palindrome of " + phrase;
+    document.getElementById("answerPalindrome").innerHTML = revPhrase + " is not a palindrome of " + phrase;
   }
 }
